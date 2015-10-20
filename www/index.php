4e268245c73339bdd31ea5a66050468222826f54
dominiclord <dlord@outlook.com>
2015-10-19 20:59:00 -0400

    Candidates

--- www/index.php
+++ www/index.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * We need open data canada.gov, kthx
+ * We need open data canada.ca, kthx
  *
  * @author Dominic Lord <dlord@outlook.com>
  * @copyright 2015 dominiclord
@@ -21,23 +21,38 @@ $app = new Slim([
 ]);
 
 // Basically all the data from SRC/CBC
+function fetch_data() {
+    $url = 'http://electr.trafficmanager.net/dare';
 
-$url = 'http://electr.trafficmanager.net/dare';
+    $connection = curl_init();
+    curl_setopt($connection, CURLOPT_URL, $url);
+    curl_setopt($connection, CURLOPT_RETURNTRANSFER, 1);
 
-$connection = curl_init();
-curl_setopt($connection, CURLOPT_URL, $url);
+    $headers = [
+        'Host: electr.trafficmanager.net',
+        'Origin: http://ici.radio-canada.ca',
+        'Referer: http://ici.radio-canada.ca/resultats-elections-canada-2015/'
+    ];
 
-$headers = [
-    'Host: electr.trafficmanager.net',
-    'Origin: http://ici.radio-canada.ca',
-    'Referer: http://ici.radio-canada.ca/resultats-elections-canada-2015/'
-];
+    curl_setopt($connection, CURLOPT_HTTPHEADER, $headers);
 
-curl_setopt($connection, CURLOPT_HTTPHEADER, $headers);
+    $data = curl_exec($connection);
 
-$data = curl_exec($connection);
+    curl_close($connection);
 
-curl_close($connection);
+    if (empty($data)) {
+        $data = file_get_contents('sample.json');
+    }
+
+    return $data;
+}
+
+$data = fetch_data();
+
+// Basic interface
+$app->get('/', function ( ) use ($app, $data) {
+    echo 'Hey';
+});
 
 /**
  * Main API group
@@ -52,10 +67,17 @@ $app->group('/api', function () use ($app, $data) {
      * @param $app   Application
      * @param $data  ALL DATA
      */
-    $app->get('/candidates', function () use ($app, $data) {
+    $app->get('/candidates/', function () use ($app, $data) {
+
+        $data = json_decode($data);
+
         $app->response()->headers->set('Content-Type', 'application/json');
         $app->response()->setStatus(200);
-        echo json_encode($data);
+
+        //var_dump($data);
+        echo json_encode($data->C);
     });
 
 });
+
+$app->run();
